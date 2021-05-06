522ffa19c8a7e41fa7bd5c885824ce0ab1ea23ca
mhassanist <msaudi.cse@gmail.com>
2021-05-06 23:13:36 +0300

    applied code review comments

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -12,7 +12,7 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     start_node  =  &m_Model.FindClosestNode(start_x,start_y);
     end_node    =  &m_Model.FindClosestNode(end_x,end_y);
 
-    start_node->g_value = start_node->distance(*start_node);
+    start_node->g_value = 0;
     start_node->h_value = CalculateHValue(start_node);
 }
 
